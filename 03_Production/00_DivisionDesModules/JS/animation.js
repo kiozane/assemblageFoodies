401c0f291354a7667a847f63f39f8dc78c04db5f
samuel <samuel.leonard1999@hotmail.com>
2020-04-24 14:49:30 -0400

    commit JqueryV2

--- 03_Production/00_DivisionDesModules/JS/animation.js
+++ 03_Production/00_DivisionDesModules/JS/animation.js
@@ -1,5 +1,19 @@
 $(function(){
-    $('i').on('click', function(){
-        $('ul').toggleClass('menuMobile');
-    });
+    /* [isMobile] */
+    if ($('nav').hasClass('isMobile')){
+        $('.fas').on('focus', function(){
+             
+            switchMenu();
+        }); 
+        $('.fas').on('blur', function(){
+            switchMenu();
+        }); 
+    }
 });
+
+
+
+function switchMenu(){
+    $('nav').toggleClass('menuFerme');
+    $('nav').toggleClass('menuOuvert');
+}
